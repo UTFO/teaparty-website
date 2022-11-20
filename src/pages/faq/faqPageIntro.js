691edb557f0c2ca717865aaff799961c754a03f4
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2022-11-20 01:09:52 -0500

    Started the intro design
    Started the intro design of the page. Will finish it and the rest of the page tomorrow. Questions are now padded to the left of the board.

---
+++ src/pages/faq/faqPageIntro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function faqIntro(){
+    return(
+        <div className="intro">
+            <h1>WELCOME TO OUR FAQ PAGE</h1>
+            <h4>This page contains the answers to our frequently answered questions.</h4>
+        </div>
+    );
+}
+
+export default faqIntro;
