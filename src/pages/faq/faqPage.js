fa4c85c1e4be287f6a3091bb33b4f90e62c65dd4
FakeDeepLearner <89857498+FakeDeepLearner@users.noreply.github.com>
2022-11-21 07:19:56 -0500

    Page intro
    Adjusted the positions of the page intro text and the question component

--- src/pages/faq/faqPage.js
+++ src/pages/faq/faqPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import QuestionComponent from './QuestionComponent';
+import FaqIntro from './faqPageIntro';
+import "./faqPage.css";
 
 function FAQ() {
-    return (<>
-        <QuestionComponent/>
-    </>)
+    return (
+        <div className='page'>
+            <FaqIntro/>
+            <QuestionComponent/>
+        </div>
+    );
 }
 
 export default FAQ;
--- src/pages/faq/faqPageIntro.js
+++ src/pages/faq/faqPageIntro.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./fagPageIntro.css";
 
-function faqIntro(){
+function FaqIntro(){
     return(
         <div className="intro">
             <h1>WELCOME TO OUR FAQ PAGE</h1>
-            <h4>This page contains the answers to our frequently answered questions.</h4>
+            <h4>This page contains the answers to our frequently answered questions. These will be regularly updated with time. We suggest you 
+                to check this page before asking your own question.
+            </h4>
         </div>
     );
 }
 
-export default faqIntro;
+export default FaqIntro;
