e362ac82d061af34e9bc9b97ca535539363c6e1d
Irwin <irwinngo@gmail.com>
2022-12-27 01:57:09 -0500

    Completed, requires database before further progress

--- src/page-components/events/eventsPage.js
+++ src/page-components/events/eventsPage.js
@@ -1,23 +1,56 @@
 import './eventsPage.css';
 
+var socialColor = '#74cdef';
+var teaColor = '#62ff8b';
+var gameColor = '#f26c4f';
+var speakerColor = '#f7941d';
 
+var events = require('./toDeprecate/events.json').events; //To replace with the actual database
+
+//Returns a color based on type of event
+function ColorSelect(type) {
+    switch(type) {
+        case "social": return socialColor;
+            break;
+        case "tea": return teaColor;
+            break;
+        case "game": return gameColor;
+            break;
+        case "speaker": return speakerColor;
+            break;
+        default: return 'blue';
+
+    }
+}
+
+//The template for a listed event
 function EventForm(props) {
     return (
         <div className="page-events-board-slot">
+            <div className="page-events-date" style={{backgroundColor: ColorSelect(props.type)}}>{props.date}</div>
+            <div className="page-events-info">
+                <p id="page-events-name" style={{color: ColorSelect(props.type)}}>{props.name}</p>
+                <p id="page-events-time">{props.time} | {props.address}</p>
+            </div>
         </div>
     )
 }
 
+//Generates a list of EventForm modules
+function EventGenerate() {
+    return events.map((event) => {
+        return <EventForm name={event.name}  date={event.date} time={event.time} address={event.address} type={event.type}/> //Format subject to change
+    });
+}
+
+//The main event section
 function Events() {
     
     return <>
     
         <div className="page-events-board">
             <div className="page-events-board-container">
-                <EventForm name="A" date="NOV 18" time="14:32 PM" address="Hart House"/>
-                <EventForm name="A" date="NOV 18" time="14:32 PM" address="Hart House"/>
-                <EventForm name="A" date="NOV 18" time="14:32 PM" address="Hart House"/>
-                <EventForm name="A" date="NOV 18" time="14:32 PM" address="Hart House"/>
+                {EventGenerate()}
             </div>
         </div>
 
