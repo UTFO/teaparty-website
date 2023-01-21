bd79883c65aacaf8261b2db2765caa811598ec06
MauoHardy <113133868+MauoHardy@users.noreply.github.com>
2023-01-21 14:07:28 -0500

    Add files via upload

---
+++ src/page-components/home/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+  {
+    image: require('./images/slide1.png'),
+    header: 'Header',
+    txt: 'This is the text for slide1'
+  },
+  {
+    image: require('./images/slide2.jpg'),
+    header: 'Header',
+    txt: 'This is the text for slide2'
+  },
+  {
+    image: require('./images/slide3.jpg'),
+    header: 'Header',
+    txt: 'This is the text for slide3'
+  },
+];
