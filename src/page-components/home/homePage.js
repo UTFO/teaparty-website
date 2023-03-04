01bb41fd1ac9b60fc799c4f1ffd735daf075c9c7
Irwin <irwinngo@gmail.com>
2023-03-04 16:51:32 -0500

    Added Docker File

--- src/page-components/home/homePage.js
+++ src/page-components/home/homePage.js
@@ -27,27 +27,27 @@ function Home() {
                 <path d="M-100,-37 C2,55 450,55 550,-37 L550,55 L-100,55" fill="#c4df9b"/>
             </svg>
             <div className="colorFill">
-            <h1 className="event-header">
-                Check out our event highlights!
-            </h1>
-            <Carousel
-                fullHeightHover={false}
-                navButtonsProps={{
-                    style: {
-                        backgroundColor: 'transparent',
-                        borderRadius: 0,
-                        
+                <h1 className="event-header">
+                    Check out our event highlights!
+                </h1>
+                <Carousel
+                    fullHeightHover={false}
+                    navButtonsProps={{
+                        style: {
+                            backgroundColor: 'transparent',
+                            borderRadius: 0,
+                            
+                        }
+                    }}
+                    animation={"slide"}
+                    navButtonsAlwaysVisible={true}
+                    NextIcon={<img src={Arrow_Icon} className='next'/>}
+                    PrevIcon={<img src={Arrow_Icon} className="prev"/>}>
+                    {
+                        SliderData.map((item,index) => <ImageSlider data={item}/>)
                     }
-                }}
-                animation={"slide"}
-                navButtonsAlwaysVisible={true}
-                NextIcon={<img src={Arrow_Icon} className='next'/>}
-                PrevIcon={<img src={Arrow_Icon} className="prev"/>}>
-                {
-                    SliderData.map((item,index) => <ImageSlider data={item}/>)
-                }
-            </Carousel>
-            <div className="slider"></div>
+                </Carousel>
+                <div className="slider"></div>
             </div>
            <svg viewBox="0 0 450 25"> 
              <path d="M-100,70 C2,-22 450,-22 550,70 L 550,-22 L -100, -22" fill="#c4df9b"/>
@@ -65,7 +65,7 @@ function Home() {
         <div className='page-home-sponsor'>
             <h2>Interested in Sponsoring? <img src={Cup2} height={30}></img></h2>
             <p>Send us an email &nbsp; <a href="mailto:irwinngo@gmail.com" target="_blank" rel="noreferrer">
-            <img src={Email} height={20}></img></a> &nbsp; or message us on instagram! &nbsp;<a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <img src={Email} height={20}></img></a> &nbsp; or message us on instagram! &nbsp;<a href="https://www.instagram.com/uoftea.party/" target="_blank" rel="noreferrer">
             <img src={Insta} height={20}></img></a></p>
         </div>
     </div>
