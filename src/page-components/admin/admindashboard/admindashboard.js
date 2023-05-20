b65d17d708909e299d7fb98135c4a66a34ecf65b
Irwin <irwinngo@gmail.com>
2023-05-20 01:32:18 -0400

    Added Navbar and Dashboard & Documents page. Updated pages routes

---
+++ src/page-components/admin/admindashboard/admindashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./admindashboard.css"
+
+import AdminNavbar from '../components/nav'
+
+const AdminDashboard = () => {
+  return (
+    <div>
+        <AdminNavbar/>
+    </div>
+  )
+}
+
+export default AdminDashboard
---
+++ src/page-components/admin/admindoc/admindoc.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./admindoc.css"
+
+const AdminDoc = () => {
+  return (
+    <div>admindoc</div>
+  )
+}
+
+export default AdminDoc
