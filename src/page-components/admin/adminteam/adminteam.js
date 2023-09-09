7bbab04bddb89daaaee370ff0bfbdb7aae29f826
Irwin <irwinngo@gmail.com>
2023-09-09 14:08:21 -0400

    fix: fixed styling and changed scroll format to grid format.

--- src/page-components/admin/adminteam/adminteam.js
+++ src/page-components/admin/adminteam/adminteam.js
@@ -24,7 +24,7 @@ const AdminTeam = () => {
         ];
       });
 
-      setTeams(tempTeams);
+      setTeams([...tempTeams]);
     });
   };
 
--- src/page-components/admin/components/scrollContainer/aboutListContainer.js
+++ src/page-components/admin/components/scrollContainer/aboutListContainer.js
@@ -2,19 +2,19 @@ import "./aboutListContainer.css"
 
 function AboutListContaniner(props){
     return (
-    <div className="container">
+    <div className="admin-about-container">
         <div className="header">
-            <div classname = "title">{props.name}</div>
-            <div classname = "buttons">
-                <button classname="edit-button" onClick={props.editFunction}>
+            <div className = "admin-about-title">{props.name}</div>
+            <div className = "buttons">
+                <button className="edit-button" onClick={props.editFunction}>
                     <img src="/pencil.png"></img>
                 </button>
-                <button className="trash-button" onClick={props.deleteFunction}>
+                <button className="delete-button" onClick={props.deleteFunction}>
                     <img src="/trash.png"></img>
                 </button>
             </div>
         </div>
-        <div className="text">
+        <div className="admin-about-text">
             {props.text}
         </div>
     </div>
